b999d1a127f3cf7f6cddfb94ec524fd2e6a79149
ombasaabuya <ombasabuya@gmail.com>
2023-02-11 17:04:26 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,17 @@
 
 
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title=title
+    end
+    def turn_page
+      puts  "Flipping the page...wow, you read fast!" 
+    end
+end
+
+first_book=Book.new("Mastering Ruby")
+first_book.author ="Josphat Kiplimo"
+first_book.genre="Ruby"
+first_book.page_count=300
+first_book.turn_page  
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,17 @@
+
 # Make your shoe class here!
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+    def initialize(brand)
+        @brand=brand
+        @condition="tattered"
+    end
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition="new"
+    end
+end
+shoe = Shoe.new("leopard")
+shoe.color = "black"
+shoe.size = 43
+shoe.condition = "brandNew"
